d4b8d3e3fb055368f094df1239dd4992bd143067
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-02-17 11:40:21 +0100

    Reorder GameCard components to display game name below the critic score

--- rawg-client/src/components/GameCard.tsx
+++ rawg-client/src/components/GameCard.tsx
@@ -12,14 +12,14 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card overflow="hidden" borderRadius="10">
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+      <CardBody>     
         <HStack justifyContent="space-between">
         <PlatformIconsList
           platforms={game.parent_platforms.map((p) => p.platform)}
         />
-        <CriticScore score={game.metacritic}/>
+        <CriticScore score={game.metacritic}/>       
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
