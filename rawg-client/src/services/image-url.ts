45e3a1d319a5b0f5cd349580cb715a0ea06a1c07
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-02-17 11:38:20 +0100

    Add image cropping utility and update GameCard to use cropped images

---
+++ rawg-client/src/services/image-url.ts
@@ -0,0 +1,8 @@
+const getCroppedImageUrl = (url: string | null) => {
+    if (!url) {
+        return "";
+    } 
+    return url.replace("media/games", "media/crop/600/400/games");
+}
+
+export default getCroppedImageUrl;
