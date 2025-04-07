f10097fd9f3aa84492d9ac011c897998d8fd7e98
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-04-07 11:43:28 +0200

    Refactor App component for improved readability and structure; add usePlatform hook for platform selection logic

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,36 +1,34 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import useGenres from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import useStores from "./hooks/useStores";
 import CustomList from "./components/CustomList";
-import useGenres from "./hooks/useGenres";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 import useGameQueryStore from "./state";
 
-
 function App() {
-
-const { genreId, storeId } = useGameQueryStore((s) => s.gameQuery);
-
-const setStoreId = useGameQueryStore((s) => s.setStoreId);
-const setGenreId = useGameQueryStore((s) => s.setGenreId);
+  const { genreId, storeId } = useGameQueryStore((s) => s.gameQuery);
+  const setGenreId = useGameQueryStore((s) => s.setGenreId);
+  const setStoreId = useGameQueryStore((s) => s.setStoreId);
 
   return (
     <Grid
+      paddingX="4"
       templateAreas={{
         base: `"header" "main"`,
         lg: `"header header" "aside main"`,
       }}
       templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
-      <GridItem pl="2" area={"header"}>
+      <GridItem area={"header"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" area={"aside"}>
-           <CustomList
+        <GridItem area={"aside"}>
+          <CustomList
             title="Genres"
             onSelectedItemId={setGenreId}
             selectedItemId={genreId}
@@ -41,21 +39,18 @@ const setGenreId = useGameQueryStore((s) => s.setGenreId);
             onSelectedItemId={setStoreId}
             selectedItemId={storeId}
             useDataHook={useStores}
-          /> 
+          />
         </GridItem>
       </Show>
-      <GridItem pl="2" area={"main"}>
-        <Box>
+      <GridItem area={"main"}>
+        <Box paddingLeft={2}>
           <GameHeading />
           <HStack>
-            <PlatformSelector 
-            />
-            <SortSelector            
-            />
+            <PlatformSelector />
+            <SortSelector />
           </HStack>
+          <GameGrid />
         </Box>
-        <GameGrid
-        />
       </GridItem>
     </Grid>
   );
