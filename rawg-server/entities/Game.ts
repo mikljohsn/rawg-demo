51aa225e8b50cc5f937b9594f0e461c117bb53ff
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-04-28 11:56:08 +0200

    Add Publisher entity and establish many-to-many relationship with Game; update gameService to include publishers in getGame query

--- rawg-server/entities/Game.ts
+++ rawg-server/entities/Game.ts
@@ -1,40 +1,54 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToMany,
-    JoinTable,
-  } from "typeorm";
-  import { Genre } from "./Genre";
-  import { ParentPlatform } from "./ParentPlatform";
-  import { Store } from "./Store";
-  
-  @Entity("games")
-  export class Game {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    name: string;
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
+import { Genre } from "./Genre";
+import { ParentPlatform } from "./ParentPlatform";
+import { Store } from "./Store";
+import { Publisher } from "./Publisher";
 
-    @Column({ type: "text", nullable: true })
-    description_raw?: string;
-  
-    @Column({ nullable: true })
-    metacritic?: number;
-  
-    @Column({ nullable: true })
-    background_image?: string;
-  
-    @ManyToMany(() => Genre, (genre) => genre.games)
-    @JoinTable()
-    genres: Genre[];
-  
-    @ManyToMany(() => ParentPlatform, (platform) => platform.games)
-    @JoinTable()
-    parent_platforms: ParentPlatform[];
-  
-    @ManyToMany(() => Store, (store) => store.games)
-    @JoinTable()
-    stores: Store[];
-  }
+@Entity("games")
+export class Game {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: "text", nullable: true })
+  description_raw?: string;
+
+  @Column({ nullable: true })
+  metacritic?: number;
+
+  @Column({ nullable: true })
+  released?: string;
+
+  @Column({ type: "float", nullable: true })
+  rating?: number;
+
+  @Column({ nullable: true })
+  added?: number;
+
+  @Column({ nullable: true })
+  background_image?: string;
+
+  @ManyToMany(() => Genre, (genre) => genre.games)
+  @JoinTable()
+  genres: Genre[];
+
+  @ManyToMany(() => ParentPlatform, (platform) => platform.games)
+  @JoinTable()
+  parent_platforms: ParentPlatform[];
+
+  @ManyToMany(() => Store, (store) => store.games)
+  @JoinTable()
+  stores: Store[];
+
+  @ManyToMany(() => Publisher, (publisher) => publisher.games)
+  @JoinTable()
+  publishers: Publisher[];
+}
